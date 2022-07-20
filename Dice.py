07e72b1683c66ea853c2806d32d4cfa43390e7be
Bartek <bartus.nowaczyk@gmail.com>
2022-07-20 15:06:19 +0000

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -1,7 +1,22 @@
 import random
+
+# TODO check if int
+# TODO gracze >= 1 and gracze <= 6
+# TODO while not, keep asking
 gracze = int(input("Ile ma być graczy?"))
 if gracze >= 6:
     print("Nie moze byc wiecej graczy niz 6.")
-else:
-    for i in range(gracze):
-        dice = random.randint(1, 6)
+
+
+# we know that gracze >=1 and gracze <= 6 and it's an int
+
+# TODO loop over all gracze
+for gracz in range(gracze):
+    print("Gracz #", gracz)
+
+# if gracze == 2:
+#     dice1 = input("Podaj rzut kostki pierwszego gracza: ")
+# dice2 = input("Podaj rzut kostki drugiego gracza: ")
+# dice3 = input("Podaj rzut kostki trzeciego gracza: ")
+
+    
